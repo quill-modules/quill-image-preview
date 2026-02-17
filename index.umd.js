daf2454a94f486db8399349b7563ee916a49b8b5
zzxming <74341337+zzxming@users.noreply.github.com>
2026-02-17 06:04:22 +0000

    Deploying to gh-pages from @ quill-modules/quill-image-preview@eb6f5ed70e736b0c5ca4f1c250cd20242f83ab94 🚀

--- index.umd.js
+++ index.umd.js
@@ -1,49 +1,7 @@
-(function(global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ?  factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory((global.QuillImagePreview = {})));
-})(this, function(exports) {
-"use strict";
-Object.defineProperty(exports, '__esModule', { value: true });
-
-//#region src/index.ts
-var QuillImagePreview = class {
-	options;
-	constructor(quill, options) {
-		this.quill = quill;
-		this.options = this.resolveOptions(options);
-		this.quill.root.addEventListener("click", this.imagePreviewCheck);
-	}
-	resolveOptions(options) {
-		return Object.assign({
-			enableOnEdit: true,
-			beforePreviewDisplay: () => false
-		}, options);
-	}
-	imagePreviewCheck = (e) => {
-		if (this.options.enableOnEdit && !this.quill.isEnabled()) return;
-		const target = e.target;
-		if (target && target.tagName === "IMG" && !this.options.beforePreviewDisplay.call(this, target)) this.createImagePreview(target);
-	};
-	createImagePreview(img) {
-		const temp = document.createElement("div");
-		temp.innerHTML = `
+(function(e,t){typeof exports==`object`&&typeof module<`u`?t(exports):typeof define==`function`&&define.amd?define([`exports`],t):(e=typeof globalThis<`u`?globalThis:e||self,t(e.QuillImagePreview={}))})(this,function(e){Object.defineProperties(e,{__esModule:{value:!0},[Symbol.toStringTag]:{value:`Module`}});var t=class{options;constructor(e,t){this.quill=e,this.options=this.resolveOptions(t),this.quill.root.addEventListener(`click`,this.imagePreviewCheck)}resolveOptions(e){return Object.assign({enableOnEdit:!0,beforePreviewDisplay:()=>!1},e)}imagePreviewCheck=e=>{if(!this.options.enableOnEdit&&this.quill.isEnabled())return;let t=e.target;t&&t.tagName===`IMG`&&!this.options.beforePreviewDisplay.call(this,t)&&this.createImagePreview(t)};createImagePreview(e){let t=document.createElement(`div`);t.innerHTML=`
       <div class="fixed top-0 left-0 right-0 bottom-0 z-500 backdrop-blur-7">
         <div class="absolute top-0 left-0 right-0 bottom-0 bg-black:50 z--1" />
-        <img src="${img.src}" class="max-w-screen max-h-screen w-full h-full object-contain">
+        <img src="${e.src}" class="max-w-screen max-h-screen w-full h-full object-contain">
       </div>
-    `;
-		const wrapper = temp.children[0];
-		wrapper.addEventListener("click", () => {
-			document.body.removeChild(wrapper);
-		});
-		document.body.appendChild(wrapper);
-	}
-};
-var src_default = QuillImagePreview;
-
-//#endregion
-exports.QuillImagePreview = QuillImagePreview
-exports.default = src_default
-});
+    `;let n=t.children[0];n.addEventListener(`click`,()=>{document.body.removeChild(n)}),document.body.appendChild(n)}};e.QuillImagePreview=t,e.default=t});
 //# sourceMappingURL=index.umd.js.map
